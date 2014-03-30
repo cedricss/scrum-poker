c113094198dd90d2250c98e0cd28b008fd095d73
Cedric Soulas <soulascedric@gmail.com>
2014-03-30 16:18:09 +0200

    Move server game logic to controller folder

--- server.js
+++ server.js
@@ -22,70 +22,9 @@ require('./lib/config/express')(app);
 // Routing
 require('./lib/routes')(app);
 
-var games_state = {};
+var socketHandler = require('./lib/controllers/game');
 
-io.sockets.on('connection', function(socket) {
-  var room = "empty";
-
-  function syncPlayers() {
-    var players = {};
-    var game = {};
-    io.sockets.clients(room).forEach(function(client) {
-      if(!client.playerdisconnected) {
-        players[client.playerid] = {id:client.playerid, username:client.username, vote:client.vote};
-      }
-    });
-    if(typeof games_state[room] === "undefined") {
-      games_state[room] = {playing:false};
-    }
-    game.players = players;
-    game.state = games_state[room];
-    console.log(game);
-    io.sockets.in(room).emit("hello", game);
-  }
-
-  socket.on('hello', function(player) {
-    room = player.room;
-    socket.join(room);
-    socket.username = player.username;
-    socket.playerid = player.id;
-    socket.vote = 0;
-    syncPlayers();
-  });
-
-  socket.on('vote', function(vote) {
-    if (room) {
-      socket.vote = vote.value;
-      socket.broadcast.to(room).emit('vote', vote);
-    }else{
-      console.log("no room");
-    }
-  });
-
-  socket.on('start', function(data) {
-    if (room) {
-      games_state[room].playing = true;
-      socket.broadcast.to(room).emit('start', data);
-    }else{
-      console.log("no room");
-    }
-  });
-  
-  socket.on('stop', function(data) {
-    if (room) {
-      games_state[room].playing = false;
-      socket.broadcast.to(room).emit('stop', data);
-    }else{
-      console.log("no room");
-    }
-  });
-
-  socket.on('disconnect', function() {
-    socket.playerdisconnected = true;
-    syncPlayers();
-  });
-
-});
+io.sockets.on('connection', function(socket) { socketHandler(io, socket) });
 
 // Start server
 server.listen(config.port, function () {
